de006f18ae291ed7f71181642859663e63dd4f78
naokinagaishi-ffs <38636999+naokinagaishi-ffs@users.noreply.github.com>
2018-09-11 00:54:25 +0900

    とりあえず

--- Practice-PocketMoney/DataAccess.cpp
+++ Practice-PocketMoney/DataAccess.cpp
@@ -4,6 +4,7 @@
 #include "DataAccess.h"
 #include "FFGWorker.h"
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 //コンストラクタ
@@ -41,18 +42,49 @@ void DataAccess::CreatWorkerData()
 void DataAccess::ReadCSV(string* filePath)
 {
 	//仮実装
-	workerArray = new FFGWorker[2];//[1]で例外とぶのはなぜ？？
-	workerArray[0].name = "aaa";
-	workerArray[0].iD = "id01";
-	workerArray[0].pocketMoney = 1000;
-	workerArray[1].name = "bbb";
-	workerArray[1].iD = "id02";
-	workerArray[1].pocketMoney = 4000;
+	//workerArray = new FFGWorker[2];//[1]で例外とぶのはなぜ？？
+	//workerArray[0].name = "aaa";
+	//workerArray[0].iD = "id01";
+	//workerArray[0].pocketMoney = 1000;
+	//workerArray[1].name = "bbb";
+	//workerArray[1].iD = "id02";
+	//workerArray[1].pocketMoney = 4000;
 
 	//dataCount = sizeof(workerArray)/sizeof(workerArray[0]);//上手く行かない
 	dataCount = 2;
 
 	//string a = *filePath;
+	std::ifstream ifs;  // ファイル読み取り用ストリーム
+	ifs.open(*filePath);	// ファイルオープン
+
+	if (ifs.fail()) {	// ファイルオープンに失敗したらそこで終了
+		std::cerr << "ファイルを開けません\n";
+		exit(1);
+	}
+
+	char buf[256];	// データ一時保管用配列
+
+	int linenum = 0; // データの行数を数える
+
+	while (ifs.getline(buf, sizeof(buf))) {	// ファイルから1行ずつ読み込む
+		linenum++;	// 行数をカウントしている
+	}
+
+	std::cerr << "読み込んだ行数 = " << linenum << "\n";
+
+	ifs.clear(); // ファイル末尾に到達というフラグをクリア
+	ifs.seekg(0, std::ios::beg);	// ファイル先頭に戻る
+
+	char* lineArray;
+	lineArray = new char[linenum];	// 行数 linenum 分の配列を動的に確保
+
+	for (int i = 0; i<linenum; i++) {
+		ifs.getline(buf, sizeof(buf));	// 一行読み込んで…
+		char* a = buf;
+		lineArray[i] = *buf;// それを配列に格納
+	}
+
+	// これで、arr[0]～arr[linenum-1] に計 linenum 個のデータが格納された
 
 }
 
